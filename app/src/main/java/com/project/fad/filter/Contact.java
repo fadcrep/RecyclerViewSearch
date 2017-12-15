8f28b25a8f5887de8f63732ce076998adc817019
Crepin Fadjo <crepin.fadjo@qanbio.com>
2017-12-15 14:30:31 +0100

    Initial commit

---
+++ app/src/main/java/com/project/fad/filter/Contact.java
@@ -0,0 +1,27 @@
+package com.project.fad.filter;
+
+/**
+ * Created by Crepin Hugues FADJO (f@dcrepin) on 14/12/2017.
+ */
+
+public class Contact {
+
+    String name;
+    String image;
+    String phone;
+
+    public Contact() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+}
